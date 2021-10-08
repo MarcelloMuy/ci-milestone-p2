120d921c2a6bdf0363d1cb34aa9778e99269991a
MarcelloMuy <marcellomuylaert@icloud.com>
2021-10-08 19:16:13 +0000

    Add function to create a new paragraph after Instructions button is clicked. Add fuction to reaload the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
 
+
+/**
+ * Create a new paragraph with the instructions and a button to reload the page
+ */
+function runInstructions() {
+    let newP = document.createElement('p');
+    newP.innerHTML = "This is the Instruction...";
+    let instructionsDiv = document.getElementById('instructionsDiv');
+    instructionsDiv.appendChild(newP);
+    let button = document.getElementById('instructions'); 
+    button.remove();
+    let closeInstructions = document.createElement('button');
+    closeInstructions.innerHTML = "X";
+    instructionsDiv.appendChild(closeInstructions);
+    closeInstructions.setAttribute('onclick', 'reloadPage()');
+}
+
+/**
+ * Reloads the page
+ */
+function reloadPage() {
+    window.location.reload();
+}
