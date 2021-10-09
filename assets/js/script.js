b56d5d95083adeaf7b0788ec465ff8f846d81098
MarcelloMuy <marcellomuylaert@icloud.com>
2021-10-09 15:59:50 +0000

    Add function to display a response message for the feedback form. Add a function to handle the form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,13 +27,14 @@ function runFeedback() {
     newForm.setAttribute('id', 'feedbackForm');
     newForm.setAttribute('method', 'POST')
     newForm.setAttribute('action', 'https://formdump.codeinstitute.net/');
+    newForm.addEventListener('submit', handleSubmit);
     newForm.innerHTML = `
     <div>
         <h2 aria-label="Form Heading">Give Your Feedback</h2>
     </div>
     <div>
         <label for="fname">First Name:</label>
-        <input id="fname" name="first_name" type="text" placeholder="Enter first name" aria-label="Enter first name" required>
+        <input id="fname" name="fname" type="text" placeholder="Enter first name" aria-label="Enter first name" required>
     </div>
     <div>
         <label for="lname">Last Name:</label>
@@ -60,6 +61,40 @@ function runFeedback() {
     closeInstructions.innerHTML = "X";
     feedbackForm.appendChild(closeInstructions);
     closeInstructions.setAttribute('onclick', 'reloadPage()');
+}
+
+/**
+ * Handle the submit event of the form
+ * Display a pop up interacting with the user
+ * Call a function to submit the form after the user have closed the feedback Popup
+ * It was designed this way so the popup and the code institute form dump could be visualized separately   
+ */
+function handleSubmit(event) {
+    event.preventDefault();
+    
+    let newForm = document.getElementById('feedbackForm');
+    
+    let fname = newForm.elements['fname'].value;
+    let lname = newForm.elements['lname'].value;
+    let message =`
+    <p> Hi ${fname} ${lname}! Thank you for your feedback.</p>`
+    let feedbackPopup = document.getElementById('feedback-popup');
+    feedbackPopup.innerHTML = message;
+    let closeFeedbackPopup = document.createElement('button');
+    closeFeedbackPopup.innerHTML = "X";
+    closeFeedbackPopup.setAttribute('onclick', 'formSubmit()')
+    feedbackPopup.appendChild(closeFeedbackPopup);
+    let fullForm = document.getElementById('feedbackDiv');
+    fullForm.style.display = "none";
+}
+
+/**
+ * Submit the feedback form
+ * Will be called when the user close the feedback Popup
+ */
+function formSubmit() {
+    let newForm = document.getElementById('feedbackForm');
+    newForm.submit();
 }
 
 /**
