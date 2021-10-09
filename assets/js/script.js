d25ffe92a485c4274a278985626387da5dc90acc
MarcelloMuy <marcellomuylaert@icloud.com>
2021-10-09 13:46:02 +0000

    Add Function to generate a form when feedback is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
 
 /**
- * Create a new paragraph with the instructions and a button to reload the page
+ * Creates a new paragraph with the instructions and a button to reload the page
  */
 function runInstructions() {
     let newP = document.createElement('p');
@@ -13,6 +13,49 @@ function runInstructions() {
     let closeInstructions = document.createElement('button');
     closeInstructions.innerHTML = "X";
     instructionsDiv.appendChild(closeInstructions);
+    closeInstructions.setAttribute('onclick', 'reloadPage()');
+}
+
+/**
+ * Creates a form when the feedback button is clicked
+ * Creates a submit button
+ * Creates a button to reload the page
+ * 
+ */
+function runFeedback() {
+    let newForm = document.createElement('form');
+    newForm.setAttribute('method', 'post')
+    newForm.innerHTML = `
+    <div>
+        <h2 aria-label="Form Heading">Give Your Feedback</h2>
+    </div>
+    <div>
+        <label for="fname">First Name:</label>
+        <input id="fname" name="first_name" type="text" placeholder="Enter first name" aria-label="Enter first name" required>
+    </div>
+    <div>
+        <label for="lname">Last Name:</label>
+        <input id="lname" name="lname" type="text" placeholder="Enter last name" aria-label="Enter last name" required>
+    </div>
+    <div>
+        <label for="email">Email Adress:</label>
+        <input type="email" id="email" name="email_address" placeholder="Enter e-mail" aria-label="Enter e-mail" required>       
+    </div>
+    <div>
+        <label for="feedback"></label>
+        <textarea id="feedback" name="feedback" placeholder="Your feedback..." rows="10" cols="38" aria-label="Enter text here"></textarea>
+    </div>` 
+    let feedbackDiv = document.getElementById('feedbackDiv');
+    feedbackDiv.appendChild(newForm);
+
+    let button = document.getElementById('feedback'); 
+    button.remove();
+    let submitButton = document.createElement('button');
+    submitButton.innerHTML = "Submit";
+    feedbackDiv.appendChild(submitButton);
+    let closeInstructions = document.createElement('button');
+    closeInstructions.innerHTML = "X";
+    feedbackDiv.appendChild(closeInstructions);
     closeInstructions.setAttribute('onclick', 'reloadPage()');
 }
 
